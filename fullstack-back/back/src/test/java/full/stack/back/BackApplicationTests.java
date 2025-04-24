c9c6252114e393e680e8c338a20c79214b3ae487
Flavio <flaviolunaferreira@gmail.com>
2025-04-24 18:19:46 -0300

    iniciando projeto com configuração básica spring

---
+++ fullstack-back/back/src/test/java/full/stack/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package full.stack.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
